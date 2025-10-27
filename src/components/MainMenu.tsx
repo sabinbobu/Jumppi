969460618ed1242c9d7600a150d385d807a0fffc
sabinbobu <bobu8sabin8@gmail.com>
2025-10-27 15:49:20 +0200

    First build has completed successfully

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,4 +1,4 @@
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
 
 export const MainMenu = ({ onPlay }: { onPlay: () => void }) => {
